04ae23ed125470a3d4580612f8d7b5dc811c6f82
HuyVo <huy.vminh71@gmail.com>
2021-09-26 17:53:08 +0300

    add app-card base

---
+++ src/components-base/app-card/app-card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AppCard extends Vue {}
+</script>
+
+<style scoped></style>
